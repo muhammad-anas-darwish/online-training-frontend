ee1457e680361e801c715847533daac176920773
Muhammad Anas Darwish <muhammadanasdarwish1@gmail.com>
2025-06-09 01:03:47 +0300

    Install vue dev  tools

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import autoImport from "unplugin-auto-import/vite";
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue(),
     tailwindcss(),
     autoImport({
